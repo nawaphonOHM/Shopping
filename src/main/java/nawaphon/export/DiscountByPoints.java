9b80d8bd16880ab37abcff31b67fee65bffc79af
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-18 23:41:04 +0700

    Add `points` field and constructor to `DiscountByPoints` class
    (cherry picked from commit 03249788d0b7864132ecbaf994e34c01add79ac5)

--- src/main/java/nawaphon/export/DiscountByPoints.java
+++ src/main/java/nawaphon/export/DiscountByPoints.java
@@ -2,4 +2,10 @@ package nawaphon.export;
 
 
 public class DiscountByPoints extends Campaignable {
+
+    private final float points;
+
+    public DiscountByPoints(float points) {
+        this.points = points;
+    }
 }

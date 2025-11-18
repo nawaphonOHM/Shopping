a8c805cd7ea3a6cbb3e80d35732f8cc945f4d34a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-18 23:37:41 +0700

    Validate `amount` field in `PercentageDiscountByItem` with `@Min` annotation
    (cherry picked from commit cfdb8f19cccdcd4f211643ff78a756d58cc24a20)

--- src/main/java/nawaphon/export/PercentageDiscountByItem.java
+++ src/main/java/nawaphon/export/PercentageDiscountByItem.java
@@ -1,6 +1,7 @@
 package nawaphon.export;
 
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public class PercentageDiscountByItem extends Campaignable {
@@ -8,6 +9,7 @@ public class PercentageDiscountByItem extends Campaignable {
     @NotNull
     private final Category category;
 
+    @Min(0)
     private final float amount;
 
     public PercentageDiscountByItem(Category category, float amount) {

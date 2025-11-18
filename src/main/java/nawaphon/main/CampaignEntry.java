bb696dc3e65b7ab8515924c75f669dab60a2e1d0
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-18 22:14:02 +0700

    Introduce `CampaignEntry` class in `main` package
    (cherry picked from commit 4b9f4777359262db3994975742f8030d92064874)

---
+++ src/main/java/nawaphon/main/CampaignEntry.java
@@ -0,0 +1,16 @@
+package nawaphon.main;
+
+
+
+import nawaphon.export.Campaignable;
+
+import java.util.List;
+
+public class CampaignEntry {
+
+    private final List<Campaignable> campaignables;
+
+    public CampaignEntry(List<Campaignable> campaignables) {
+        this.campaignables = campaignables;
+    }
+}

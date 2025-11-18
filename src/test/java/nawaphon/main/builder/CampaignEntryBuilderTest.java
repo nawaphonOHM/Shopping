1e1eb505c5634961bfb1d6847369fb77f20b98be
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-18 22:31:04 +0700

    Add `CampaignEntryBuilderTest` class with initial `build` test method
    (cherry picked from commit 0d6faac1637f966c95f9b6735eb12169326fa115)

---
+++ src/test/java/nawaphon/main/builder/CampaignEntryBuilderTest.java
@@ -0,0 +1,13 @@
+package nawaphon.main.builder;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class CampaignEntryBuilderTest {
+
+    @Test
+    void shouldAddCampaignableFail() {
+    }
+}

19d258edf3b13e5519c1ce8c7d56cedba4792898
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-18 18:50:52 +0700

    Add `amount` field and constructor to `FixAmountCampaign` class
    (cherry picked from commit b3e7d2044f149746acaffe1a894de0ab705e4de5)

--- src/main/java/nawaphon/export/FixAmountCampaign.java
+++ src/main/java/nawaphon/export/FixAmountCampaign.java
@@ -1,5 +1,10 @@
 package nawaphon.export;
 
 
-public class FixAmountCampaign extends Campaignable {
+public class FixAmountCampaign extends Campaignable  {
+    private final float amount;
+
+    public FixAmountCampaign(float amount) {
+        this.amount = amount;
+    }
 }

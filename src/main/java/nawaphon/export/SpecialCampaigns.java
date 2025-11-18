c9dfee3f0ef3df8d8690d7438e17f135cc815e01
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-18 23:44:15 +0700

    Add `everyBaht` field and constructor to `SpecialCampaigns` class
    (cherry picked from commit 460156cf861352cd8f7476abbe85ea45e36bb626)

--- src/main/java/nawaphon/export/SpecialCampaigns.java
+++ src/main/java/nawaphon/export/SpecialCampaigns.java
@@ -2,4 +2,10 @@ package nawaphon.export;
 
 
 public class SpecialCampaigns extends Campaignable {
+
+    private final float everyBaht;
+
+    public SpecialCampaigns(float everyBaht) {
+        this.everyBaht = everyBaht;
+    }
 }

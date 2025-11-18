0ed9e5cd0325f874244a32cb0e8e9dfde378e53a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-18 22:05:00 +0700

    Add `campaignables` list to `CampaignEntryBuilder` class
    (cherry picked from commit a6c9c9ea7d8695676a40f6268258e3045692c14e)

--- src/main/java/nawaphon/main/builder/CampaignEntryBuilder.java
+++ src/main/java/nawaphon/main/builder/CampaignEntryBuilder.java
@@ -1,5 +1,14 @@
 package nawaphon.main.builder;
 
 
+
+import nawaphon.export.Campaignable;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class CampaignEntryBuilder {
+
+    private final List<Campaignable> campaignables = new ArrayList<>();
+
 }

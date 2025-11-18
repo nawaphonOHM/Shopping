752818f72d79fe6adf82addf2fdd94f96e57e906
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-18 22:36:47 +0700

    Add validation test for negative `amount` in `FixAmountCampaign` within `CampaignEntryBuilderTest`
    (cherry picked from commit 4c2578af63c46253c95b05586eaafa342eb20357)

--- src/test/java/nawaphon/main/builder/CampaignEntryBuilderTest.java
+++ src/test/java/nawaphon/main/builder/CampaignEntryBuilderTest.java
@@ -1,6 +1,9 @@
 package nawaphon.main.builder;
 
+import jakarta.validation.ValidationException;
+import nawaphon.export.FixAmountCampaign;
 import org.junit.jupiter.api.Test;
+
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,5 +12,9 @@ class CampaignEntryBuilderTest {
 
     @Test
     void shouldAddCampaignableFail() {
+        final var builder = CampaignEntryBuilder.getBuilder();
+
+        assertThrowsExactly(ValidationException.class, () ->  builder.addCampaignable(new FixAmountCampaign(-9.0f)));
+
     }
 }

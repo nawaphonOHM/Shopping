141cc929d3c53de23d675a63dfaeab455f13358a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-18 23:32:39 +0700

    Add percentage field and constructor to `PercentageDiscountCampaign`
    (cherry picked from commit 7169e4328b0c36ec33fc84c8281c7e8bf2f95918)

--- src/main/java/nawaphon/export/PercentageDiscountCampaign.java
+++ src/main/java/nawaphon/export/PercentageDiscountCampaign.java
@@ -2,4 +2,10 @@ package nawaphon.export;
 
 
 public class PercentageDiscountCampaign extends Campaignable {
+
+    private final float percentage;
+
+    public PercentageDiscountCampaign(float percentage) {
+        this.percentage = percentage;
+    }
 }

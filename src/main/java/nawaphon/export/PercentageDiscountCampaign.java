4af92ddd01911dc8ff508f729a1a475e50bb640a
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-18 23:32:59 +0700

    Validate percentage field in `PercentageDiscountCampaign` with `@Min` annotation
    (cherry picked from commit efb34b2116e2368b0d79cd2ad136abdf615b3e7a)

--- src/main/java/nawaphon/export/PercentageDiscountCampaign.java
+++ src/main/java/nawaphon/export/PercentageDiscountCampaign.java
@@ -1,8 +1,12 @@
 package nawaphon.export;
 
 
+import jakarta.validation.constraints.Min;
+
 public class PercentageDiscountCampaign extends Campaignable {
 
+
+    @Min(0)
     private final float percentage;
 
     public PercentageDiscountCampaign(float percentage) {

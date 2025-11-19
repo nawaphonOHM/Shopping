aadb2b3342dff52d12aef94149d661e8638b213e
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 23:13:32 +0700

    Update `ConflictCampaignIdException` to include detailed campaign context in the error message
    (cherry picked from commit f5f026bac2ac8837bcb47d35cb73c88647c3de05)

--- src/main/java/nawaphon/internal/ConflictCampaignIdException.java
+++ src/main/java/nawaphon/internal/ConflictCampaignIdException.java
@@ -2,7 +2,13 @@ package nawaphon.internal;
 
 
 public class ConflictCampaignIdException extends RuntimeException {
-    public ConflictCampaignIdException(String message) {
-        super(message);
+    public ConflictCampaignIdException(String campaignCategory, int expectedCampaignId, int actualCampaignId) {
+        super(String.format("""
+                    Expected only one campaign with same category and id.
+                    Category: %s
+                    Id: %s
+                    
+                    but got %s
+                    """, campaignCategory, expectedCampaignId, actualCampaignId));
     }
 }

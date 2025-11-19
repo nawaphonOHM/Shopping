39bb725faf94823f741d450668f107a4ac40c895
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 23:37:09 +0700

    Complete `shopping2` test with item and percentage discount campaign setup, and add total price assertion
    (cherry picked from commit 90dfe119e08948270ab20de712dd19cd699459d5)

--- src/test/java/nawaphon/main/ShoppingTest.java
+++ src/test/java/nawaphon/main/ShoppingTest.java
@@ -3,6 +3,7 @@ package nawaphon.main;
 import nawaphon.export.Category;
 import nawaphon.export.FixAmountCampaign;
 import nawaphon.export.Item;
+import nawaphon.export.PercentageDiscountCampaign;
 import nawaphon.main.builder.CampaignEntryBuilder;
 import nawaphon.main.builder.ItemCardBuilder;
 import org.junit.jupiter.api.Assertions;
@@ -33,7 +34,19 @@ class ShoppingTest {
 
     @Test
     void shopping2() {
+        final var shopping = new Shopping(
+                ItemCardBuilder.getBuilder().addItem(
+                                new Item(Category.CLOTHING, "T-shirt", 350.0f, 1)
+                        ).addItem(
+                                new Item(Category.CLOTHING, "Hat", 250.0f, 1)
+                        )
+                        .build(),
+                CampaignEntryBuilder.getBuilder().addCampaignable(new PercentageDiscountCampaign(10.0f)).build()
+        );
 
+        shopping.process();
+
+        Assertions.assertEquals(540, shopping.getTotalPrice());
     }
 
 }

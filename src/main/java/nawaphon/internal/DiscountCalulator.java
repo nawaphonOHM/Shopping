0a04ef31d7d2e408d3c67c5435071a8281025854
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 21:36:40 +0700

    Implement discount calculation for `PercentageDiscountCampaign` in `DiscountCalulator` class
    (cherry picked from commit 0254197d2b9753bc39f40b3422d554a6a0e98b7c)

--- src/main/java/nawaphon/internal/DiscountCalulator.java
+++ src/main/java/nawaphon/internal/DiscountCalulator.java
@@ -3,9 +3,13 @@ package nawaphon.internal;
 
 import nawaphon.export.Campaignable;
 import nawaphon.export.FixAmountCampaign;
+import nawaphon.export.PercentageDiscountCampaign;
 import nawaphon.main.CampaignEntry;
 import nawaphon.main.ItemCartEntry;
 import org.jetbrains.annotations.NotNull;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public abstract class DiscountCalulator {
 
@@ -21,6 +25,21 @@ public abstract class DiscountCalulator {
 
         if (campaign instanceof FixAmountCampaign) {
             return new DiscountReceived(((FixAmountCampaign) campaign).getAmount());
+        }
+
+        if (campaign instanceof PercentageDiscountCampaign) {
+            var total = BigDecimal.ZERO;
+
+            for (final var iterator = itemCart.iterator(); iterator.hasNext(); ) {
+                var item = iterator.next();
+
+                total = total.add(new BigDecimal(item.getTotalPrice()));
+            }
+
+            final var remaining = new BigDecimal(((PercentageDiscountCampaign) campaign).getPercentage())
+                    .divide(BigDecimal.valueOf(100), 2, RoundingMode.HALF_UP);
+
+            return new DiscountReceived(total.multiply(remaining).floatValue());
         }
 
         return null;

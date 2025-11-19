9e12cff3353f56f681cc8a4e425122b033c9c2dd
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 19:36:08 +0700

    Annotate `FixAmountCampaign` with `@Order`
    (cherry picked from commit 7a51e03a8ad49d48df325e8416c5ee8f3895e982)

--- src/main/java/nawaphon/export/FixAmountCampaign.java
+++ src/main/java/nawaphon/export/FixAmountCampaign.java
@@ -3,8 +3,10 @@ package nawaphon.export;
 
 import jakarta.validation.constraints.Min;
 import nawaphon.internal.CampaignCategory;
+import nawaphon.internal.Order;
 
 @CampaignCategory(category = "COUPON", id = 1)
+@Order(1)
 public class FixAmountCampaign extends Campaignable {
 
     @Min(0)

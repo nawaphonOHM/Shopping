24ca6f626281b1b89ea65dc12808dc0f6598933d
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 21:14:34 +0700

    Add `sortedCampaign` method stub to `DiscountCalculator` class
    (cherry picked from commit 0175477940381d924224155e4883fe40c06e7f59)

--- src/main/java/nawaphon/internal/DiscountCalulator.java
+++ src/main/java/nawaphon/internal/DiscountCalulator.java
@@ -1,5 +1,13 @@
 package nawaphon.internal;
 
 
+import nawaphon.main.CampaignEntry;
+
 public abstract class DiscountCalulator {
+
+
+    public static boolean sortedCampaign(CampaignEntry campaign) {
+        return false;
+    }
+
 }

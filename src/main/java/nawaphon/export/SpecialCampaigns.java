7850ca75c886c566ac0aab9b0cb46b2d875fe358
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 19:38:16 +0700

    Annotate `SpecialCampaigns` with `@Order`
    (cherry picked from commit 70a638ce99f4ce4efc9d661d12f86c5f0032a1a4)

--- src/main/java/nawaphon/export/SpecialCampaigns.java
+++ src/main/java/nawaphon/export/SpecialCampaigns.java
@@ -3,9 +3,11 @@ package nawaphon.export;
 
 import jakarta.validation.constraints.Min;
 import nawaphon.internal.CampaignCategory;
+import nawaphon.internal.Order;
 
 
 @CampaignCategory(category = "SEASONAL", id = 1)
+@Order(3)
 public class SpecialCampaigns extends Campaignable {
 
     @Min(0)

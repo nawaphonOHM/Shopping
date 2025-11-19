4c397f7b0ad4ac527f7199b8d9406a2f92f67805
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 23:34:52 +0700

    Complete `shopping1` test with item and campaign setup, and add total price assertion
    (cherry picked from commit c5c357309a3a40bbdf13101e800856e2e16219a0)

--- src/test/java/nawaphon/main/ShoppingTest.java
+++ src/test/java/nawaphon/main/ShoppingTest.java
@@ -1,5 +1,11 @@
 package nawaphon.main;
 
+import nawaphon.export.Category;
+import nawaphon.export.FixAmountCampaign;
+import nawaphon.export.Item;
+import nawaphon.main.builder.CampaignEntryBuilder;
+import nawaphon.main.builder.ItemCardBuilder;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 
@@ -7,6 +13,21 @@ class ShoppingTest {
 
     @Test
     void shopping1() {
+
+        final var shopping = new Shopping(
+                ItemCardBuilder.getBuilder().addItem(
+                                new Item(Category.CLOTHING, "T-shirt", 350.0f, 1)
+                        ).addItem(
+                                new Item(Category.CLOTHING, "Hat", 250.0f, 1)
+                        )
+                        .build(),
+                CampaignEntryBuilder.getBuilder().addCampaignable(new FixAmountCampaign(50.0f)).build()
+        );
+
+        shopping.process();
+
+        Assertions.assertEquals(550, shopping.getTotalPrice());
+
 
     }
 

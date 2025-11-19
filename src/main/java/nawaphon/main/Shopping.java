27417b05db63459767a0b18d8bce014da5615052
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 20:52:25 +0700

    Inject `CampaignEntry` dependency via constructor in `Shopping` class.
    (cherry picked from commit 02e2926295234ddb38140669832bba32bc1d9612)

--- src/main/java/nawaphon/main/Shopping.java
+++ src/main/java/nawaphon/main/Shopping.java
@@ -4,8 +4,10 @@ package nawaphon.main;
 public class Shopping {
 
     private final ItemCartEntry itemCartEntry;
+    private final CampaignEntry campaignEntry;
 
-    public Shopping(ItemCartEntry itemCartEntry) {
+    public Shopping(ItemCartEntry itemCartEntry, CampaignEntry campaignEntry) {
         this.itemCartEntry = itemCartEntry;
+        this.campaignEntry = campaignEntry;
     }
 }

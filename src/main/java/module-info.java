bb0e1f6cc4eb4ca0655f6d592ac4676fb081adbb
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 23:52:05 +0700

    Export `nawaphon.export` and `nawaphon.internal` packages in `module-info.java`
    (cherry picked from commit 3050575d92f15b326e4c3ad59a0dcd6623a4fc09)
    (cherry picked from commit 193f4a6f25c0ac619839c570d5bae233dc399018)

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,4 +3,7 @@ module Shopping {
     requires jakarta.validation;
     requires org.apache.commons.lang3;
     requires org.jetbrains.annotations;
+
+    exports nawaphon.export;
+    exports nawaphon.internal;
 }

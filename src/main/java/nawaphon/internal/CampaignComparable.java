206f34919df1f63695f03cc9e2a15bd20481ee05
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 20:07:01 +0700

    Update `compare` method in `CampaignComparable` to sort `Campaignable` by `@Order` value
    (cherry picked from commit e7b9de6ce7b8381f9044c8bdb57b26e8b2c11e7c)

--- src/main/java/nawaphon/internal/CampaignComparable.java
+++ src/main/java/nawaphon/internal/CampaignComparable.java
@@ -2,12 +2,16 @@ package nawaphon.internal;
 
 
 import nawaphon.export.Campaignable;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Comparator;
 
 public class CampaignComparable implements Comparator<Campaignable> {
     @Override
-    public int compare(Campaignable campaignable, Campaignable t1) {
-        return 0;
+    public int compare(@NotNull Campaignable campaignable, @NotNull Campaignable t1) {
+        final var annotation1 = campaignable.getClass().getAnnotation(Order.class).value();
+        final var annotation2 = t1.getClass().getAnnotation(Order.class).value();
+
+        return annotation1 - annotation2;
     }
 }

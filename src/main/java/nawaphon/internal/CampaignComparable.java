06745be9d5d36653fe38bc60d2e300058b3acb43
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 20:05:32 +0700

    Implement `Comparator` in `CampaignComparable` for `Campaignable` comparison
    (cherry picked from commit d7b47a771ab73541be2b8f453fe209fda34d9498)

--- src/main/java/nawaphon/internal/CampaignComparable.java
+++ src/main/java/nawaphon/internal/CampaignComparable.java
@@ -1,5 +1,13 @@
 package nawaphon.internal;
 
 
-public class CampaignComparable {
+import nawaphon.export.Campaignable;
+
+import java.util.Comparator;
+
+public class CampaignComparable implements Comparator<Campaignable> {
+    @Override
+    public int compare(Campaignable campaignable, Campaignable t1) {
+        return 0;
+    }
 }

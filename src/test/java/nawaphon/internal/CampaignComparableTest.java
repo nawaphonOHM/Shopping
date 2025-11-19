d95b77a4964e8c2e79c4b80c75b140b9669c0ca9
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 20:13:23 +0700

    Add test scaffold for `shouldReturnZeroNumber` in `CampaignComparableTest`
    (cherry picked from commit ecbcf66748a7f9e636356153d1b1f749ee751b09)

--- src/test/java/nawaphon/internal/CampaignComparableTest.java
+++ src/test/java/nawaphon/internal/CampaignComparableTest.java
@@ -1,8 +1,15 @@
 package nawaphon.internal;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 
 class CampaignComparableTest {
+
+    @Test
+    void shouldReturnZeroNumber() {
+
+    }
 
 }

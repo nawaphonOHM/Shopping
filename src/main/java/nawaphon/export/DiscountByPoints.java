294f1b23bad7cb8e434e4ae05817ba812ffbb521
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 19:37:55 +0700

    Annotate `DiscountByPoints` with `@Order`
    (cherry picked from commit be7603686687e9391607b0f5ddebd7dbc96b3870)

--- src/main/java/nawaphon/export/DiscountByPoints.java
+++ src/main/java/nawaphon/export/DiscountByPoints.java
@@ -3,9 +3,11 @@ package nawaphon.export;
 
 import jakarta.validation.constraints.Min;
 import nawaphon.internal.CampaignCategory;
+import nawaphon.internal.Order;
 
 
 @CampaignCategory(category = "ON_TOP", id = 2)
+@Order(2)
 public class DiscountByPoints extends Campaignable {
 
     @Min(0)

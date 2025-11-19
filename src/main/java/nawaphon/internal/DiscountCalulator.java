c8222614168e9729f2c8723c1396ab4fb61cbca0
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 21:15:06 +0700

    Implement `sortedCampaign` method in `DiscountCalulator` class with null-safety.
    (cherry picked from commit b88f38a9b7fcd654d87394ff9f22bde20772400e)

--- src/main/java/nawaphon/internal/DiscountCalulator.java
+++ src/main/java/nawaphon/internal/DiscountCalulator.java
@@ -2,12 +2,16 @@ package nawaphon.internal;
 
 
 import nawaphon.main.CampaignEntry;
+import org.jetbrains.annotations.NotNull;
 
 public abstract class DiscountCalulator {
 
 
-    public static boolean sortedCampaign(CampaignEntry campaign) {
-        return false;
+    public static boolean sortedCampaign(@NotNull CampaignEntry campaign) {
+
+        campaign.sortedBy(new CampaignComparable());
+
+        return true;
     }
 
 }

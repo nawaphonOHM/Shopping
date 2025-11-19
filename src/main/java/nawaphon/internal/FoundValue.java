19e1a7fa4059f96d9616fc5f2517b942c290a9ee
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 19:46:44 +0700

    Override `equals` and `hashCode` methods in `FoundValue` class
    (cherry picked from commit 28db5f8a39976e4b3ce308ae86e3935c692db9d0)

--- src/main/java/nawaphon/internal/FoundValue.java
+++ src/main/java/nawaphon/internal/FoundValue.java
@@ -1,11 +1,25 @@
 package nawaphon.internal;
 
 
+import java.util.Objects;
+
 public class FoundValue {
 
     private final int value;
 
     public FoundValue(int value) {
         this.value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        FoundValue that = (FoundValue) o;
+        return value == that.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(value);
     }
 }

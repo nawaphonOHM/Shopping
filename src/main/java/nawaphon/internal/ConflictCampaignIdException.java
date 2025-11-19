837033691eb84e5e5f7051567c0d81472681b853
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 23:12:22 +0700

    Introduce `ConflictCampaignIdException` in `internal` package
    (cherry picked from commit d5193cea862ffc2569c7569f7ae2886bbb848789)

---
+++ src/main/java/nawaphon/internal/ConflictCampaignIdException.java
@@ -0,0 +1,8 @@
+package nawaphon.internal;
+
+
+public class ConflictCampaignIdException extends RuntimeException {
+    public ConflictCampaignIdException(String message) {
+        super(message);
+    }
+}

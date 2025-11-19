1f6d0e6c1dcacda991384627437bba2a545adf26
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 20:45:24 +0700

    Annotate `getBuilder()` method in `ItemCardBuilder` with `@Contract` and `@NotNull`.
    (cherry picked from commit 4d0c605b5d34fb9df5e19c5465122981cec436e0)

--- src/main/java/nawaphon/main/builder/ItemCardBuilder.java
+++ src/main/java/nawaphon/main/builder/ItemCardBuilder.java
@@ -2,13 +2,16 @@ package nawaphon.main.builder;
 
 
 import nawaphon.internal.ItemCart;
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
 
 public class ItemCardBuilder {
 
     private final ItemCart itemCart = new ItemCart();
 
-    public static ItemCardBuilder getBuilder() {
-        return null;
+    @Contract(" -> new")
+    public static @NotNull ItemCardBuilder getBuilder() {
+        return new ItemCardBuilder();
     }
 
 }

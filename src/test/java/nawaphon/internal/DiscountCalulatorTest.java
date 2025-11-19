9e13778f858d8955c56d6ee3ecd0381f4e489c5e
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 22:26:05 +0700

    Implement and test fixed amount discount calculation in `DiscountCalulatorTest`
    (cherry picked from commit 2b1f543082e792e3795b2dee2e83171be73076da)

--- src/test/java/nawaphon/internal/DiscountCalulatorTest.java
+++ src/test/java/nawaphon/internal/DiscountCalulatorTest.java
@@ -1,12 +1,30 @@
 package nawaphon.internal;
 
 
+import nawaphon.export.Category;
+import nawaphon.export.FixAmountCampaign;
+import nawaphon.export.Item;
+import nawaphon.main.builder.ItemCardBuilder;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class DiscountCalulatorTest {
 
     @Test
     void calculateFixAmountDiscount() {
+
+        final var itemCart = ItemCardBuilder.getBuilder().addItem(
+                new Item(Category.CLOTHING, "T-shirt", 350.0f, 1)
+        ).addItem(
+                new Item(Category.CLOTHING, "Hat", 250.0f, 1)
+                )
+                .build();
+
+        final var campaign = new FixAmountCampaign(50.0f);
+
+        DiscountCalulator.calculateDiscount(itemCart, campaign);
+
+        Assertions.assertEquals(50, DiscountCalulator.calculateDiscount(itemCart, campaign).discount());
 
     }
 

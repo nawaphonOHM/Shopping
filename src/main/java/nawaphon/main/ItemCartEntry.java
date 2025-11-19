e6a3a13e0ff9bf9f1bb86cc5e212c66f6026e333
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 20:49:42 +0700

    Initialize `ItemCart` in `ItemCartEntry` class constructor
    (cherry picked from commit f608307c02ade367ddca1d3fc696f03fa7a6d60b)

--- src/main/java/nawaphon/main/ItemCartEntry.java
+++ src/main/java/nawaphon/main/ItemCartEntry.java
@@ -1,5 +1,13 @@
 package nawaphon.main;
 
 
+import nawaphon.internal.ItemCart;
+
 public class ItemCartEntry {
+
+    private final ItemCart itemCart;
+
+    public ItemCartEntry(ItemCart itemCart) {
+        this.itemCart = itemCart;
+    }
 }

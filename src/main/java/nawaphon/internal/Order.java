8be13ec70057512bb6af0e31ed8a42052106ce21
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 19:29:04 +0700

    Add `@Retention(RetentionPolicy.RUNTIME)` to `@Order` annotation
    (cherry picked from commit d775092974fdbe48a8056b2f38352a0aa422166e)

--- src/main/java/nawaphon/internal/Order.java
+++ src/main/java/nawaphon/internal/Order.java
@@ -1,6 +1,10 @@
 package nawaphon.internal;
 
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Order {
 
     int value();

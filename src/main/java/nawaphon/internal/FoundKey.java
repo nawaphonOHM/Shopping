9c3b68b70bf7a13f2144c61d7fce959307e3294b
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 19:43:31 +0700

    Override `equals` and `hashCode` methods in `FoundKey` class
    (cherry picked from commit bb3b40af54dcd5085666f94fd37eaf91ab0afcd5)

--- src/main/java/nawaphon/internal/FoundKey.java
+++ src/main/java/nawaphon/internal/FoundKey.java
@@ -1,11 +1,25 @@
 package nawaphon.internal;
 
 
+import java.util.Objects;
+
 public class FoundKey {
 
     private final String key;
 
     public FoundKey(String key) {
         this.key = key;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        FoundKey foundKey = (FoundKey) o;
+        return Objects.equals(key, foundKey.key);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(key);
     }
 }

079130c309c4bd45fb3fa63f59c7754f8f942199
Nawaphon Isarathanachaikul <nawaphon2539@gmail.com>
2025-11-19 20:14:52 +0700

    Add test for `CampaignComparable` comparison logic in `shouldReturnNegativeNumber`
    (cherry picked from commit 852d1494a6c916e0488385e6e953813b8211975a)

--- src/test/java/nawaphon/internal/CampaignComparableTest.java
+++ src/test/java/nawaphon/internal/CampaignComparableTest.java
@@ -1,5 +1,6 @@
 package nawaphon.internal;
 
+import nawaphon.export.FixAmountCampaign;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,6 +10,12 @@ class CampaignComparableTest {
 
     @Test
     void shouldReturnZeroNumber() {
+        final var campaignable1 = new FixAmountCampaign(10.0f);
+        final var campaignable2 = new FixAmountCampaign(11.0f);
+
+        final var comparator = new CampaignComparable();
+
+        assertEquals(0, comparator.compare(campaignable1, campaignable2), "Should be equal");
 
     }
 
